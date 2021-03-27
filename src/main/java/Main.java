bc6ea70cb6a3cc742b63d7fefa497a261abc4b62
Vansovich Aleksey <251091@niuitmo.ru>
2021-03-27 13:07:14 +0300

    Made a few changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         MathSystems systems = new MathSystems();
-        systems.getResult(8, 0.00001);
+        double limit = Math.PI/2 - 0.1;
+        for (double i = - limit; i < limit; i += 0.1) {
+            systems.getResult(i, 0.01);
+        }
+        Writer.close();
     }
 }
